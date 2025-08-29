6ec7a593d5e0208fb6d8690bde77bf7ddc1370cc
Silvia Fang <fang.silvia@gmail.com>
2025-08-29 12:46:25 -0700

    feat(badge): add MLH 2026 badge

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,30 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="apple-mobile-web-app-title" content="WildHacks" />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <a
+          id="mlh-trust-badge"
+          style={{
+            display: "block",
+            maxWidth: "100px",
+            minWidth: "60px",
+            position: "fixed",
+            right: "50px",
+            top: "0",
+            width: "10%",
+            zIndex: "10000",
+          }}
+          href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2026-season&utm_content=gray"
+          target="_blank"
+        >
+          <img
+            src="https://s3.amazonaws.com/logged-assets/trust-badge/2026/mlh-trust-badge-2026-gray.svg"
+            alt="Major League Hacking 2026 Hackathon Season"
+            style={{ width: "100%" }}
+          />
+        </a>
+      </body>
     </html>
   );
 }
