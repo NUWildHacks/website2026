78f3548270750d82cf1f11ba7feda2346bc076d8
Melinda Chang <melindachang.hy@gmail.com>
2026-01-15 04:44:08 -0600

    feat: set up project layout

---
+++ src/components/layout/SmoothLayout.tsx
@@ -0,0 +1,64 @@
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { ScrollSmoother } from 'gsap/ScrollSmoother';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useRef } from 'react';
+
+type SmoothLayoutProps = {
+  children: React.ReactNode;
+  world: React.ReactNode;
+  scenes: React.ReactNode;
+};
+
+export const SmoothLayout = (
+  { children, world, scenes }: SmoothLayoutProps,
+) => {
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  const contentRef = useRef<HTMLDivElement>(null);
+  const skewRef = useRef<HTMLDivElement>(null);
+  const smoother = useRef<ScrollSmoother>(null);
+
+  useGSAP(() => {
+    smoother.current = ScrollSmoother.create({
+      speed: 0.6,
+      smooth: 0.8,
+      ease: 'power3.out',
+      effects: true,
+      normalizeScroll: true,
+      wrapper: wrapperRef.current,
+      content: contentRef.current,
+    });
+
+    const skewTo = gsap.quickTo(skewRef.current, 'skewY', {
+      duration: 0.3,
+      ease: 'power3.out',
+    });
+
+    ScrollTrigger.create({
+      onUpdate: (self) => {
+        let velocity = self.getVelocity() / 750;
+        velocity = gsap.utils.clamp(-7, 7, velocity);
+        skewTo(velocity);
+      },
+    });
+
+    gsap.set(skewRef.current, {
+      transformOrigin: 'center center',
+      force3D: true,
+    });
+  }, { scope: wrapperRef });
+
+  return (
+    <div ref={wrapperRef} id='smooth-wrapper'>
+      <div ref={contentRef} id='smooth-content'>
+        {world}
+        <div id='scene-layer'>
+          {scenes}
+        </div>
+        <div ref={skewRef}>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
