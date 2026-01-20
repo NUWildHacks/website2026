bdf2094733ed8e8c6561c5503d7f8ac0c055d3bf
Silvia Fang <fang.silvia@gmail.com>
2026-01-19 22:07:56 -0600

    fix: spacing

--- src/features/footer/Footer.tsx
+++ src/features/footer/Footer.tsx
@@ -3,9 +3,7 @@ import styles from './Footer.module.scss';
 function Footer() {
   return (
     <div className={styles.footer}>
-      {
-        // <h1 className={styles.footer__heading}>WILDHACKS</h1>
-      }
+      {<h1 className={styles.footer__heading}>FAQs</h1>}
     </div>
   );
 }
