6a1e328873310e191a353ca48c0cc8a5236deb10
Silvia Fang <fang.silvia@gmail.com>
2026-01-20 00:30:27 -0600

    feat(footer): added footer text

--- src/features/footer/Footer.tsx
+++ src/features/footer/Footer.tsx
@@ -1,5 +1,7 @@
+import ClockBreak from '@/assets/clock.webp';
 import { categoryMeta, type FaqCategory, faqs } from '@/data/faqs';
 import { useMemo } from 'react';
+import { FaInstagramSquare, FaLinkedin } from 'react-icons/fa';
 import styles from './Footer.module.scss';
 import ToggleQuestion from './ToggleQuestion';
 
@@ -27,32 +29,54 @@ function Footer() {
 
   return (
     <section className={styles.footer}>
-      <h1 className={styles.footer__heading}>FAQs</h1>
+      <div className={styles.footer__container}>
+        <h1 className={styles.footer__heading}>FAQs</h1>
 
-      <div className={styles.footer__grid}>
-        {categoryOrder.map((cat) => {
-          const meta = categoryMeta[cat];
-          const items = grouped[cat];
+        <div className={styles.footer__grid}>
+          {categoryOrder.map((cat) => {
+            const meta = categoryMeta[cat];
+            const items = grouped[cat];
 
-          return (
-            <div key={cat} className={styles.footer__cell}>
-              <div className={styles.footer__meta}>
-                <span className={styles.footer__time}>{meta.time}</span>
-                <span className={styles.footer__slug}>{meta.slug}</span>
+            return (
+              <div key={cat} className={styles.footer__cell}>
+                <div className={styles.footer__meta}>
+                  <span className={styles.footer__time}>{meta.time}</span>
+                  <span className={styles.footer__slug}>{meta.slug}</span>
+                </div>
+
+                {items.map((item) => (
+                  <ToggleQuestion
+                    key={item.id}
+                    question={item.question}
+                    answer={item.answer} />
+                ))}
               </div>
+            );
+          })}
+        </div>
+      </div>
+      <img src={ClockBreak} style={{ width: '100%' }} />
 
-              {items.map((item) => (
-                <ToggleQuestion
-                  key={item.id}
-                  question={item.question}
-                  answer={item.answer} />
-              ))}
-            </div>
-          );
-        })}
+      <div className={styles.footer__teamContainer}>
+        <p>Made with &lt;3 by the WildHacks Team</p>
+        <div className={styles.footer__socialContainer}>
+          <a
+            href='https://www.linkedin.com/company/wildhacks/'
+            className={styles.footer__socialButton}>
+            <FaLinkedin size={24} />
+          </a>
+          <a
+            href='https://www.instagram.com/wildhacks/'
+            className={styles.footer__socialButton}>
+            <FaInstagramSquare size={24} />
+          </a>
+        </div>
       </div>
     </section>
   );
 }
 
 export default Footer;
+
+// https://www.linkedin.com/company/wildhacks/
+// https://www.instagram.com/wildhacks/
